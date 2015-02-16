9f431210424b081d87ec164fa176394c1ea677b4
mzilic <mzilic@mono.hr>
2015-02-16 14:04:26 +0100

    - build with comments

--- dist/baasic-angular-app-settings.js
+++ dist/baasic-angular-app-settings.js
@@ -1,45 +1,170 @@
 (function (angular, undefined) { /* exported module */
 
+    /** 
+     * @overview The angular.module is a global place for creating, registering or retrieving modules. All modules should be registered in an application using this mechanism.
+     * @copyright (c) 2015 Mono-Software
+     * @license MIT
+     * @author Mono-Software
+     */
+
+    /**
+     * An angular module is a container for the different parts of your app - services, directives etc. In order to use baasic.appSettings module functionality it must be added as a dependency to your app.
+     * @module baasic.appSettings
+     * @example
+     (function (Main) {
+     "use strict";
+     var dependencies = [
+     "baasic.api",
+     "baasic.membership",
+     "baasic.security",
+     "baasic.appSettings",
+     "baasic.article",
+     "baasic.dynamicResource",
+     "baasic.keyValue",
+     "baasic.valueSet"
+     ];
+     Main.module = angular.module("myApp.Main", dependencies);
+     }
+     (MyApp.Modules.Main = {})); 
+     */
     var module = angular.module('baasic.appSettings', ['baasic.api']);
 
     /* globals module */
+    /**
+     * @module baasicApplicationSettingsRouteService
+     **/
 
+    /** 
+     * @overview Application settings route service.
+     * @copyright (c) 2015 Mono-Software
+     * @license MIT
+     * @author Mono-Software
+     */
     (function (angular, module, undefined) {
         'use strict';
         module.service('baasicApplicationSettingsRouteService', ['baasicUriTemplateService', function (uriTemplateService) {
             return {
+                /**
+                 * Parses get route, this route doesn't expose any properties.
+                 * @method        
+                 * @example baasicLoginRouteService.get.expand({});               
+                 **/
                 get: uriTemplateService.parse('applications/{?embed,fields}'),
+                /**
+                 * Parses update route, this route doesn't expose any properties.
+                 * @method        
+                 * @example baasicLoginRouteService.update.expand({});               
+                 **/
                 update: uriTemplateService.parse('applications/'),
+                /**
+                 * Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [github](https://github.com/Baasic/uritemplate-js) page.
+                 * @method
+                 * @example uriTemplateService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
+                 **/
                 parse: uriTemplateService.parse
             };
         }]);
     }(angular, module)); /* globals module */
+    /**
+     * @module baasicApplicationSettingsService
+     **/
 
+    /** 
+     * @overview Application settings service.
+     * @copyright (c) 2015 Mono-Software
+     * @license MIT
+     * @author Mono-Software
+     */
     (function (angular, module, undefined) {
         'use strict';
         module.service('baasicApplicationSettingsService', ['baasicApiHttp', 'baasicApiService', 'baasicConstants', 'baasicApplicationSettingsRouteService', function (baasicApiHttp, baasicApiService, baasicConstants, applicationSettingsRouteService) {
             return {
                 routeService: applicationSettingsRouteService,
+                /**
+                 * Returns a promise that is resolved once the get action has been performed. Success response returns the application settings resource.
+                 * @method        
+                 * @example 
+                 baasicApplicationSettingsService.get()
+                 .success(function (data) {
+                 // perform success action here
+                 })
+                 .error(function (response, status, headers, config) {
+                 // perform error handling here
+                 });
+                 **/
                 get: function (options) {
                     return baasicApiHttp.get(applicationSettingsRouteService.get.expand(baasicApiService.getParams(options))).success(function (appSettings) {
                         appSettings.origins = appSettings.origins || [];
                     });
                 },
+                /**
+                 * Returns a promise that is resolved once the update application settings action has been performed.
+                 * @method        
+                 * @example 
+                 // Existing resource is a resource previously fetched using get action.
+                 existingResource.allowAnyOrigin = true;
+                 baasicApplicationSettingsService.update(existingResource)
+                 .success(function (data) {
+                 // perform success action here
+                 })
+                 .error(function (response, status, headers, config) {
+                 // perform error handling here
+                 });
+                 **/
                 update: function (data) {
                     var params = baasicApiService.updateParams(data);
                     var model = params[baasicConstants.modelPropertyName];
                     return baasicApiHttp.put(model.links('put').href, model);
                 },
+                /**
+                 * Returns a promise that is resolved once the remove action has been performed. If the action is successfully completed the resource is permanently removed from the system.
+                 * @method        
+                 * @example 
+                 // Existing resource is a resource previously fetched using get action.
+                 baasicUserService.remove(existingResource)
+                 .success(function (data) {
+                 // perform success action here
+                 })
+                 .error(function (response, status, headers, config) {
+                 // perform error handling here
+                 });
+                 **/
                 remove: function (data) {
                     var params = baasicApiService.removeParams(data);
                     var model = params[baasicConstants.modelPropertyName];
                     return baasicApiHttp.delete(model.links('delete').href);
                 },
+                /**
+                 * Returns a promise that is resolved once the activate action has been performed. If the action is successfully completed the application is activated.
+                 * @method        
+                 * @example 
+                 // Existing resource is a resource previously fetched using get action.
+                 baasicUserService.activate(existingResource)
+                 .success(function (data) {
+                 // perform success action here
+                 })
+                 .error(function (response, status, headers, config) {
+                 // perform error handling here
+                 });
+                 **/
                 activate: function (data) {
                     var params = baasicApiService.updateParams(data);
                     var model = params[baasicConstants.modelPropertyName];
                     return baasicApiHttp.put(model.links('activate').href);
                 },
+                /**
+                 * Returns a promise that is resolved once the deactivate action has been performed. If the action is successfully completed the application deactivated.
+                 * @method        
+                 * @example 
+                 // Existing resource is a resource previously fetched using get action.
+                 baasicUserService.deactivate(existingResource)
+                 .success(function (data) {
+                 // perform success action here
+                 })
+                 .error(function (response, status, headers, config) {
+                 // perform error handling here
+                 });
+                 **/
                 deactivate: function (data) {
                     var params = baasicApiService.updateParams(data);
                     var model = params[baasicConstants.modelPropertyName];
